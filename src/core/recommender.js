6a87d99a5678a667711f00f9e45b3788525de071
modalqa <depfaris@gmail.com>
2025-08-16 15:17:30 +0700

    change model

--- src/core/recommender.js
+++ src/core/recommender.js
@@ -4,7 +4,7 @@ async function recommendTestCases(codeSnippet) {
   const prompt = `The following code changes:\n\n${codeSnippet}\n\nPlease recommend a test case.`;
 
   const response = await axios.post(process.env.OPENROUTER_API_URL, {
-    model: "mistralai/mistral-small-3.2-24b-instruct:free",
+    model: process.env.OPENROUTER_MODEL,
     messages: [{ role: "user", content: prompt }],
   }, {
     headers: { Authorization: `Bearer ${process.env.OPENROUTER_API_KEY}` }
--- tests/analyzer.test.js
+++ tests/analyzer.test.js
@@ -1,7 +1,39 @@
 const { extractChangedFunctions } = require('../src/core/analyzer');
 
-test('extractChangedFunctions detects function lines', () => {
+test('extractChangedFunctions detects simple function declaration', () => {
   const diff = '+function test() {}\n+console.log("hello");';
   const result = extractChangedFunctions(diff);
   expect(result).toEqual(['+function test() {}']);
+});
+
+test('extractChangedFunctions detects arrow function', () => {
+  const diff = '+const sayHello = () => {\n+  console.log("Hi");\n+}';
+  const result = extractChangedFunctions(diff);
+  expect(result).toEqual(['+const sayHello = () => {']);
+});
+
+test('extractChangedFunctions detects function expression', () => {
+  const diff = '+const sum = function(a, b) {\n+  return a + b;\n+}';
+  const result = extractChangedFunctions(diff);
+  expect(result).toEqual(['+const sum = function(a, b) {']);
+});
+
+test('extractChangedFunctions ignores non-function lines', () => {
+  const diff = '+let x = 10;\n+console.log("Not a function");';
+  const result = extractChangedFunctions(diff);
+  expect(result).toEqual([]);
+});
+
+test('extractChangedFunctions handles multiple functions', () => {
+  const diff = `
++function one() {}
++const two = function() {};
++const three = () => {};
+  `;
+  const result = extractChangedFunctions(diff);
+  expect(result).toEqual([
+    '+function one() {}',
+    '+const two = function() {};',
+    '+const three = () => {};'
+  ]);
 });
